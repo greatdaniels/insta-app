8f6fca96afb2830c54ce6246333f0cc242c04f7c
Dan Njoroge <greatdaniels@gmail.com>
2020-05-31 15:18:50 +0300

    db migration

--- instagram/models.py
+++ instagram/models.py
@@ -33,18 +33,6 @@ class Profile(models.Model):
     def search_profile(cls, name):
         return cls.objects.filter(user__username__icontains=name).all()
 
-class Comment(models.Model):
-    comment = models.TextField()
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
-    user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='comments')
-    created = models.DateTimeField(auto_now_add=True, null=True)
-
-    def __str__(self):
-        return f'{self.user.name} Post'
-
-    class Meta:
-        ordering = ["-pk"]
-
 class Post(models.Model):
     image = models.ImageField(upload_to='posts/')
     name = models.CharField(max_length=250, blank=True)
@@ -74,4 +62,16 @@ class Post(models.Model):
 
     def __str__(self):
         return f'{self.user.name} Post'
+
+class Comment(models.Model):
+    comment = models.TextField()
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='comments')
+    created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return f'{self.user.name} Post'
+
+    class Meta:
+        ordering = ["-pk"]
 
