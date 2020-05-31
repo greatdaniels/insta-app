8f1cd096e46d45bf82bc6f18345be56181b3dc42
Dan Njoroge <greatdaniels@gmail.com>
2020-05-31 15:05:31 +0300

    adding class Comment to models

--- instagram/models.py
+++ instagram/models.py
@@ -33,8 +33,17 @@ class Profile(models.Model):
     def search_profile(cls, name):
         return cls.objects.filter(user__username__icontains=name).all()
 
+class Comment(models.Model):
+    comment = models.TextField()
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='comments')
+    created = models.DateTimeField(auto_now_add=True, null=True)
 
+    def __str__(self):
+        return f'{self.user.name} Post'
 
+    class Meta:
+        ordering = ["-pk"]
 
 class Post(models.Model):
     image = models.ImageField(upload_to='posts/')
