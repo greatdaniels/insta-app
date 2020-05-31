4a62fb4f817ef8a2c4b7bce7c60799080c390e5e
Dan Njoroge <greatdaniels@gmail.com>
2020-05-31 15:40:30 +0300

    trying to make tests pass and adding .gitignore

--- instagram/tests.py
+++ instagram/tests.py
@@ -5,10 +5,10 @@ from django.contrib.auth.models import User
 # Create your tests here.
 class TestProfile(TestCase):
     def setUp(self):
-        self.user = User(username='charles')
+        self.user = User(username='scott')
         self.user.save()
 
-        self.profile_test = Profile(name='image', profile_picture='default.jpg', bio='this is a test profile', user=self.user)
+        self.profile_test = Profile(id = 1, name='image', profile_picture='default.jpg', bio='this is a test profile', user=self.user)
 
     def test_instance(self):
         self.assertTrue(isinstance(self.profile_test, Profile))
@@ -17,3 +17,28 @@ class TestProfile(TestCase):
         self.profile_test.save_profile()
         after = Profile.objects.all()
         self.assertTrue(len(after) > 0)
+
+    def test_delete_profile(self):
+        self.profile_test.delete_profile()
+        after = Profile.objects.all()
+        self.assertTrue(len(after) < 1)
+
+class TestPost(TestCase):
+    def setUp(self):
+        self.profile_test = Profile(name='scott', user=User(username='doka'))
+        # self.profile_test.save()
+
+        self.image_test = Post(image='default.png', name='test', caption='default test', user=self.profile_test)
+
+    def test_insatance(self):
+        self.assertTrue(isinstance(self.image_test, Post))
+
+    def test_save_image(self):
+        self.image_test.save_image()
+        images = Post.objects.all()
+        self.assertTrue(len(images) > 0)
+
+    # def test_delete_image(self):
+    #     self.image_test.delete_image()
+    #     after = Post.objects.all()
+    #     self.assertTrue(len(after) < 1)
