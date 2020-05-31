f18c620350c5ef7d188b2a53306c7abf8ad84887
Dan Njoroge <greatdaniels@gmail.com>
2020-05-31 16:32:49 +0300

    redefining urlpatterns

--- instaclone/urls.py
+++ instaclone/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^instagram/', include('instagram.urls'))
+    path('', include('instagram.urls'))
+    # url(r'^instagram/', include('instagram.urls'))
 ]
