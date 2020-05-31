6342439debb3320a0e22e5836dcaa1837c5e1381
Dan Njoroge <greatdaniels@gmail.com>
2020-05-31 16:08:58 +0300

    adding signup and account paths in urls.py

--- instagram/urls.py
+++ instagram/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import url
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     url('^$', views.welcome, name = 'welcome'),
+    path('account/', include('django.contrib.auth.urls')),
+    path('signup/', views.signup, name='signup'),
+    path('', views.index, name = 'index'),
 ]
