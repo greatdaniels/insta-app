03c316f071b538925fe33c6558e86353ac363e3d
Dan Njoroge <greatdaniels@gmail.com>
2020-06-01 14:33:32 +0300

    adding profile route to views.py

--- instagram/views.py
+++ instagram/views.py
@@ -15,4 +15,12 @@ def index(request):
     images = Images.get_all_images()
     return render(request,'instagram/index.html',{'images':images})
 
+# @login_required(login_url='/accounts/login/')
+def profile(request, username):
+    user = User.objects.get(username=username)
+    profile = Profiles.filter_profile_by_id(user.id)
+    title = f'{user.username}\'s Profile '
+    images = Images.get_profile_images(user.id)
+    return render(request, 'profile/profile.html',{'title':title,'users':user,'profile':profile,'images':images})
+
 
