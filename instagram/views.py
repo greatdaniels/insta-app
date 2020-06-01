0109c9709602a64746c613cb098b063b02bc9f71
Dan Njoroge <greatdaniels@gmail.com>
2020-06-01 14:31:25 +0300

    adding index route to views.py

--- instagram/views.py
+++ instagram/views.py
@@ -2,11 +2,17 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.decorators import login_required
+from . models import Images, Comments, Profiles
 
 
 # Create your views here.
-@login_required(login_url='/accounts/login/')
+# @login_required(login_url='/accounts/login/')
 def welcome(request):
     return render(request, 'welcome.html')
+
+# @login_required(login_url='/accounts/login/')
+def index(request):
+    images = Images.get_all_images()
+    return render(request,'instagram/index.html',{'images':images})
 
 
