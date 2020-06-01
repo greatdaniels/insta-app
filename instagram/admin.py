cf8f047b7eb13889fe7ce28ab97e8754377df052
Dan Njoroge <greatdaniels@gmail.com>
2020-06-01 14:29:55 +0300

    adding code to admin.py

--- instagram/admin.py
+++ instagram/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Images, Profiles, Comments
 
 # Register your models here.
+admin.site.register(Profiles)
+admin.site.register(Images)
+admin.site.register(Comments)
