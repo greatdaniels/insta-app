87ee9346551e3366ef6e05b5ecb6a661162e099d
Dan Njoroge <greatdaniels@gmail.com>
2020-06-02 06:59:01 +0300

    adding view_single_image route

--- instagram/views.py
+++ instagram/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.contrib.auth import login, authenticate
 from django.contrib.auth.decorators import login_required
 from . models import Images, Comments, Profiles
+from django.contrib.auth.models import User
+from .forms import PostComment, PostImagesForm, PostProfile
 
 
 # Create your views here.
@@ -22,5 +23,22 @@ def profile(request, username):
     title = f'{user.username}\'s Profile '
     images = Images.get_profile_images(user.id)
     return render(request, 'profile/profile.html',{'title':title,'users':user,'profile':profile,'images':images})
+
+# @login_required(login_url='/accounts/login/')
+def view_single_image(request,image_id):
+    image = Images.get_image_by_id(image_id)
+    comments = Comments.get_comment_by_image(image_id)
+    current_user = request.user
+    if request.method == 'POST':
+        form = PostComment(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.image = image
+            comment.user = request.user
+            comment.save()
+            return redirect('singleImage',image_id=image_id)
+    else:
+        form = PostComment()
+    return render(request, 'photos.html',{'image':image,'form':form,'comments':comments})
 
 
