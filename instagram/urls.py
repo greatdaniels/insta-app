c7f195e28cb9462214fa58b2980c04fa1eaeb8db
Dan Njoroge <greatdaniels@gmail.com>
2020-06-03 11:26:54 +0300

    adding app urls

--- instagram/urls.py
+++ instagram/urls.py
@@ -1,7 +1,15 @@
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    url('^$', views.welcome, name = 'welcome'),
-    
+    url('^$', views.index, name = 'index'),
+    url(r'^user/(?P<username>\w+)',views.profile,name='profile'), 
+    url(r'^image/(?P<image_id>\d+)',views.view_single_image,name='singleImage'),
+    url(r'^accounts/edit/', views.edit_profile, name='edit_profile'),
+    url(r'^postImage/',views.new_image,name='postImage'), 
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
