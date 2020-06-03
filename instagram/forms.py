2c6bae7d57e0e399e46761f5705376596b037eff
Dan Njoroge <greatdaniels@gmail.com>
2020-06-03 11:31:50 +0300

    adding forms.py

--- instagram/forms.py
+++ instagram/forms.py
@@ -1,1 +1,23 @@
 from django import forms
+from .models import Profile, Comment, Image
+
+class PostImagesForm(forms.ModelForm):
+    image = forms.ImageField()
+    caption = forms.CharField(max_length=100)
+    class Meta: 
+        model = Image
+        exclude = ['posted','profile']
+        
+        
+class PostComment(forms.ModelForm):
+    class Meta: 
+        model = Comment
+        exclude = ['image','posted','user']
+
+class PostProfile(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ['user']
+        
+
+        
